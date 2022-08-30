00ae3dd1fe20d60f4abd6bf3687359eff282c928
Link <linkorgs@163.com>
2022-08-30 15:30:51 +0800

    ci: mv rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,12 @@
-import packageJson from './package.json'
 import dts from 'rollup-plugin-dts'
 import del from 'rollup-plugin-delete'
 
 export default {
   input: 'src/index.ts',
-  output: { file: packageJson.types, format: 'esm' },
+  output: { file: 'dist/index.d.ts', format: 'esm' },
   plugins: [
     dts({
-      respectExternal: true,
+      respectExternal: process.env.RESPECT_EXTERNAL === 'runtime',
     }),
     del({
       targets: 'dist',
