861cd78b51e4f7ad42de1d39df456a5cf2be06ec
Link <linkorgs@163.com>
2022-10-27 17:31:55 +0800

    type(cli): download overload

--- packages/cli/global.d.ts
+++ packages/cli/global.d.ts
@@ -1,13 +1,14 @@
 declare module 'download-git-repo' {
-  export function download(
+  function download(
     repo: string,
     dest: string,
     opts: { clone: boolean },
     fn: (err: string | null) => void
   ): void
-  export default function download(
+  function download(
     repo: string,
     dest: string,
     fn: (err: string | null) => void
   ): void
+  export default download
 }
