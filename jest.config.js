579eddb48da3c9e30cc4a4f9c4e54a512972745d
Link <13370740109@xtransfer.cn>
2023-02-06 15:59:51 +0800

    fix: ci jest test

--- jest.config.js
+++ jest.config.js
@@ -3,15 +3,16 @@ const { compilerOptions } = require('./tsconfig.json')
 
 /** @type {import('jest').Config} */
 module.exports = {
+  rootDir: process.cwd(),
   preset: 'ts-jest',
   testMatch: process.argv.includes('--colors')
     ? ['**/*.spec.ts']
     : [`${process.cwd()}/tests/**/(*.)+(spec|test).[jt]s?(x)`],
   testEnvironment: 'jsdom',
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>',
+    prefix: '<rootDir>../../',
   }),
-  collectCoverageFrom: [`${process.cwd()}/src/**/*.ts`],
+  collectCoverageFrom: [`./src/**/*.ts`],
   globals: {
     __DEV__: true,
     __WARN__: false,
