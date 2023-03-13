924a0073e62878acb6a3ed0412ce410d80319571
Link <linkorgs@163.com>
2023-03-13 15:37:53 +0800

    test(jsx): update babel jest case

--- packages/babel-plugin-jsx/tests/page.spec.tsx
+++ packages/babel-plugin-jsx/tests/page.spec.tsx
@@ -1,6 +1,38 @@
-import { createInstance, nextRender } from '@gyron/runtime'
+import { FC, useReactive, createInstance, nextRender } from 'gyron'
 import { trim } from './util'
-import { View } from './view'
+
+function ComponentA() {
+  return <span>a</span>
+}
+function ComponentB() {
+  return <span>b</span>
+}
+
+function Layout({ children, change }: any) {
+  return (
+    <div onClick={change} id="layout">
+      {children}
+    </div>
+  )
+}
+
+const View = FC(() => {
+  const page = useReactive({
+    count: 0,
+  })
+  const handleChange = () => {
+    page.count++
+  }
+  const RenderChild = () => {
+    const e = Boolean(page.count % 2)
+    return e ? <ComponentA /> : <ComponentB />
+  }
+  return (
+    <Layout change={handleChange}>
+      <RenderChild />
+    </Layout>
+  )
+})
 
 test('project magic', async () => {
   const container = document.createElement('div')
